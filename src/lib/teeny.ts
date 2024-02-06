daa9a77c9b685ac27a96a1d2dd6ee6f91adc7c37
Barrett Ruth <br.barrettruth@gmail.com>
2024-02-05 20:27:43 -0500

    feat(tensor): add, mul ops

--- src/lib/teeny.ts
+++ src/lib/teeny.ts
@@ -1,5 +1,7 @@
 import nj from "@d4c/numjs";
 
+type Kwargs = {};
+
 class Tensor {
   grad?: Tensor;
   data: nj.NdArray;
@@ -19,8 +21,39 @@ class Tensor {
   }
 
   add(tensor: Tensor) {
-    return Add.run_op(this, tensor);
+    return Add.run_op([this, tensor]);
   }
+
+  sub(tensor: Tensor) {
+    return Sub.run_op([this, tensor]);
+  }
+
+  mul(tensor: Tensor) {
+    return Mul.run_op([this, tensor]);
+  }
+
+  // reduce(fn: Fn, axis?: number | number[], keepdim = false): Tensor {
+  //   if (axis === undefined) {
+  //     axis = Array.from(Array(this.shape.length).keys());
+  //   } else if (typeof axis === "number") {
+  //     axis = [axis];
+  //   }
+  //
+  //   for (let i = 0; i < axis.length; ++i) {
+  //     if (axis[i] < 0) {
+  //       axis[i] += this.shape.length;
+  //     }
+  //   }
+  //   const shape = this.shape.filter((_, i) => !axis.includes(i));
+  //
+  //   if (this.shape.includes(0) && !this.shape.includes(0)) {
+  //     // TODO:
+  //     return;
+  //   }
+  //
+  //   const new_shape = this.shape.filter((s, i) => (axis.includes(i) ? 1 : s));
+  //   const tensor = fn.run_op([this], { new_shape });
+  // }
 
   toString() {
     let repr = `Data: ${this.data}`;
@@ -36,7 +69,7 @@ class Fn {
   requires_grad: boolean;
   parents?: Tensor[];
 
-  constructor(...tensors: Tensor[]) {
+  constructor(tensors: Tensor[]) {
     this.needs_input_grad = tensors.map((t) => t.requires_grad);
     this.requires_grad = this.needs_input_grad.some(Boolean);
     if (this.requires_grad) {
@@ -50,17 +83,19 @@ class Fn {
     );
   }
 
-  forward(...args: nj.NdArray[]): nj.NdArray {
+  forward(args: nj.NdArray[], kwargs?: Kwargs): nj.NdArray {
     throw new Error(
       `NotImplemented: forward not implemented for type ${typeof this}`
     );
   }
 
-  // TODO: missing kwargs
-  static run_op(...tensors: Tensor[]): Tensor {
-    const context = new this(...tensors);
+  static run_op(tensors: Tensor[], options: Kwargs = {}): Tensor {
+    const context = new this(tensors);
     const tensor = new Tensor(
-      context.forward(...tensors.map((t) => t.data)),
+      context.forward(
+        tensors.map((t) => t.data),
+        options
+      ),
       context.requires_grad
     );
     if (context.requires_grad) {
@@ -71,7 +106,7 @@ class Fn {
 }
 
 class Add extends Fn {
-  forward(x: nj.NdArray, y: nj.NdArray) {
+  forward([x, y]: nj.NdArray[]) {
     return x.add(y);
   }
   backward(grad_output: nj.NdArray) {
@@ -81,6 +116,40 @@ class Add extends Fn {
     ];
   }
 }
+
+class Sub extends Fn {
+  forward([x, y]: nj.NdArray[]) {
+    return x.subtract(y);
+  }
+  backward(grad_output: nj.NdArray) {
+    return [
+      this.needs_input_grad[0] ? grad_output : undefined,
+      this.needs_input_grad[1] ? grad_output.negative() : undefined,
+    ];
+  }
+}
+
+class Mul extends Fn {
+  x!: nj.NdArray;
+  y!: nj.NdArray;
+
+  forward([x, y]: nj.NdArray[]) {
+    this.x = x;
+    this.y = y;
+    return x.multiply(y);
+  }
+
+  backward(grad_output: nj.NdArray) {
+    return [
+      this.needs_input_grad[0] ? this.y.multiply(grad_output) : undefined,
+      this.needs_input_grad[1] ? this.x.multiply(grad_output) : undefined,
+    ];
+  }
+}
+
+// class Sum extends Fn {
+//   forward(tensor: Tensor, )
+// }
 
 function testTensors() {
   let t = new Tensor(nj.array([1, 2, 3, 4, 5]), true);
@@ -106,8 +175,13 @@ function testTensors() {
   );
 
   const t3 = t1.add(t);
-  console.log("added:");
-  console.log(t3);
+  const t4 = t1.sub(t);
+  // console.log("added:");
+  // console.log(t3);
+  // console.log("subbed:");
+  // console.log(t4);
+
+  console.log(t1.mul(t));
 }
 
 function main() {
