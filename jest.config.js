a43801d6591cd5c0eed32e9404ec54d486b87fee
Barrett Ruth <br.barrettruth@gmail.com>
2024-02-17 14:14:36 -0500

    fix(test): fix jest base config

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  // Optionally, specify the directory where Jest should look for tests
-  roots: ['<rootDir>/src'],
-  // Match test files
   testMatch: [
     '**/?(*.)+(spec|test).ts?(x)',
   ],
