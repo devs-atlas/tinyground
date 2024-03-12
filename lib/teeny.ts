07b828c041341fe84ada00c703372cdaec1dc949
Nicholas Hoffs <zur3hn@virginia.edu>
2024-03-12 19:15:49 -0400

    no more mlops in tensor

--- lib/teeny.ts
+++ lib/teeny.ts
@@ -1,60 +1,93 @@
 import * as tf from "@tensorflow/tfjs";
-
+import * as mlops from "./mlops";
+import LazyBuffer from "./lazy";
+import { BinaryOps, LoadOps, UnaryOps, ReduceOps } from "./ops";
 export class Tensor {
   grad?: Tensor;
-  data: tf.Tensor;
+  data: LazyBuffer;
   shape: number[];
   requires_grad: boolean;
   context?: Fn;
 
-  // TODO: add kwargs for this - check og teeny
-  full(shape: number[], fill_value: number, requires_grad: boolean) {
-    // TODO: add expand
-    return new Tensor(fill_value, requires_grad = requires_grad).expand(shape)
-  }
-
-  constructor(data: number | tf.Tensor, requires_grad: boolean) {
+  constructor(data: number | tf.Tensor | LazyBuffer, requires_grad: boolean) {
     if (data instanceof tf.Tensor) {
-      this.data = data;
+      this.data = new LazyBuffer(data);
+      this.shape = data.shape;
+    } else if (typeof data == "number") {
+      this.data = new LazyBuffer(tf.tensor([data]));
+      this.shape = [];
+    } else if (Array.isArray(data)) {
+      this.data = new LazyBuffer(tf.tensor(data));
       this.shape = data.shape;
     } else {
-      this.data = tf.tensor([data]);
-      this.shape = [];
+      this.data = data;
+      this.shape = data.shape;
     }
     this.requires_grad = requires_grad;
   }
 
+  get dtype() {
+    return this.data.dtype;
+  }
+
+  static full(shape: number[], fill_value: number, requires_grad: boolean) {
+    return new Tensor(tf.ones(shape).mul(fill_value), requires_grad);
+  }
+
+  static ones(shape: number[], requires_grad: boolean) {
+    return new Tensor(tf.ones(shape), requires_grad);
+  }
+
+  static zeros(shape: number[], fill_value: number, requires_grad: boolean) {
+    return new Tensor(tf.zeros(shape).mul(fill_value), requires_grad);
+  }
+
   add(tensor: Tensor) {
-    return Add.run_op([this, tensor]);
+    return mlops.Add.run_op([this, tensor]);
   }
 
   sub(tensor: Tensor) {
-    return Sub.run_op([this, tensor]);
+    return mlops.Sub.run_op([this, tensor]);
   }
 
   mul(tensor: Tensor) {
-    return Mul.run_op([this, tensor]);
+    return mlops.Mul.run_op([this, tensor]);
   }
 
-
-  _reduce(fxn: Fn, axis?: number[] | number, keepdim = false) {
+  _reduce(fxn: Fn, axis?: number[] | number, keepdim = false): Tensor {
+    if (!(fxn instanceof mlops.Sum || fxn instanceof mlops.Max)) {
+      throw new Error("fxn must be an instance of Sum or Max");
+    }
     let axis_ = axis;
     if (!axis_) {
       axis_ = Array.from({ length: this.shape.length }, (_, index) => index);
-    }
-    else if (typeof (axis_) === 'number') {
+    } else if (typeof axis_ === "number") {
       axis_ = [axis_];
     }
 
-    const reducedShape = this.shape.filter((_, index) => !axis_.includes(index));
+    //@ts-ignore
+    let reducedShape = this.shape.filter((_, index) => !axis_.includes(index));
 
     if (reducedShape.includes(0) && !this.shape.includes(0)) {
-      return Tensor.full(this.shape.map((s, _) => s == 0 ? 1 : 0)
+      if (keepdim) {
+        reducedShape = reducedShape.map((axis) => (axis ? axis : 1));
+      }
+      const fillVal = fxn instanceof mlops.Sum ? 0 : Infinity;
+      return Tensor.full(reducedShape, fillVal, this.requires_grad);
     }
 
-    const ret = fxn.run_op(this, { new_shape?: number[] | number })
+    let ret =
+      fxn instanceof mlops.Sum
+        ? mlops.Sum.run_op([this], { axis: axis_, keepdim })
+        : mlops.Max.run_op([this], { axis: axis_, keepdim });
+
+    return keepdim ? ret : ret.reshape(reducedShape);
   }
 
+  //TODO: do i need to restate the axis type here and _reduce? should only be needed on outward facing methods.
+  sum(axis?: number | number[], keepdim = false) {
+    return this._reduce(mlops.Sum, axis, keepdim);
+  }
   toString() {
     let repr = `Data: ${this.data}`;
     if (this.requires_grad) {
@@ -64,7 +97,7 @@ export class Tensor {
   }
 }
 
-class Fn {
+export class Fn {
   needs_input_grad: boolean[];
   requires_grad: boolean;
   parents?: Tensor[];
@@ -77,8 +110,8 @@ class Fn {
     }
   }
 
-  forward(_: any, ...__: any): any { }
-  backward(_: any, ...__: any): any { }
+  forward(_: any, ...__: any): any {}
+  backward(_: any, ...__: any): any {}
 
   static run_op(tensors: Tensor[], options = {}): Tensor {
     // TODO: can we just make this "Fn"?
@@ -97,81 +130,12 @@ class Fn {
   }
 }
 
-class Expand extends Fn {
-  input_shape!: number[];
-
-  forward([x]: tf.Tensor[], { new_shape }: { new_shape: number[] }) {
-    this.input_shape = x.shape;
-    return x.broadcastTo(new_shape);
-  }
-  backward(grad_output: tf.Tensor) {
-    // fix this - need to pass axis to
-    // return sum(grad_output, this.input_shape);
-  }
-}
-
-class Add extends Fn {
-  forward([x, y]: tf.Tensor[]) {
-    return x.add(y);
-  }
-  backward(grad_output: tf.Tensor) {
-    // o = x + y
-    // dx/do = 1 -> multiply by grad_output to represent dx/dL
-    return [
-      this.needs_input_grad[0] ? grad_output : undefined,
-      this.needs_input_grad[1] ? grad_output : undefined,
-    ];
-  }
-}
-
-class Sub extends Fn {
-  forward([x, y]: tf.Tensor[]) {
-    return x.sub(y);
-  }
-  backward(grad_output: tf.Tensor) {
-    // o = x - y = x - 1y
-    // dy/do = -1
-    return [
-      this.needs_input_grad[0] ? grad_output : undefined,
-      this.needs_input_grad[1] ? grad_output.neg() : undefined,
-    ];
-  }
-}
-
-class Mul extends Fn {
-  x!: tf.Tensor;
-  y!: tf.Tensor;
-
-  forward([x, y]: tf.Tensor[]) {
-    this.x = x;
-    this.y = y;
-    return x.mul(y);
-  }
-
-  backward(grad_output: tf.Tensor) {
-    // o = x * y
-    // dx/do = y -> treat y as constant (partial derivative)
-    return [
-      this.needs_input_grad[0] ? this.y.mul(grad_output) : undefined,
-      this.needs_input_grad[1] ? this.x.mul(grad_output) : undefined,
-    ];
-  }
-}
-
-class Reshape extends Fn {
-  input_shape!: number[];
-
-  forward([x]: tf.Tensor[], { shape }: { shape: number[] }) {
-    this.input_shape = shape!;
-    return x.reshape(this.input_shape);
-  }
-
-  backward(grad_output: tf.Tensor) {
-    let out = grad_output.reshape(this.input_shape);
-    return out;
-  }
-}
-
-class Sum extends Fn {
-  forward([x]: tf.Tensor[], { axis }: { axis?: number | number[] }) { }
-}
+let a = Tensor.zeros([3, 8, 4], 3, true);
+a.data
+  .array()
+  .then((array) => {
+    console.log(array); // This will print the actual values once the Promise is resolved.
+  })
+  .catch((error) => {
+    console.error("An error occurred:", error);
+  });
